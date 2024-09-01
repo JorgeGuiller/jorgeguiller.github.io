f55cf761b82db9c73d5db84a94a07de43415944d
nico <90756549+sengokus@users.noreply.github.com>
2024-09-01 10:56:57 +0800

    feat: updated projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,88 +1,100 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import NavBar from "@/components/common/navbar";
+import { PROJECTS } from "../utils/constants/projects";
+import { Project } from "../utils/types/projects";
+import Image from "next/image";
+
 export default function Projects() {
-  // PROJECT LIST
-  const projects = [
-    {
-      title: "DOST Customer Portal",
-      description:
-        "The DOST Region VI Customer Portal is designed to enhance customer interaction and service efficiency. It provides a user-friendly platform for customers to access various services, information, and support related to DOST Region VI.",
-      technologies: ["PHP", "Bulma CSS", "JavaScript"],
-      link: "#",
-      image: "/assets/images/cas.png",
-    },
-    {
-      title: "DOST Customer Portal",
-      description:
-        "The DOST Region VI Customer Portal is designed to enhance customer interaction and service efficiency. It provides a user-friendly platform for customers to access various services, information, and support related to DOST Region VI.",
-      technologies: ["Unity", "Textscript", "C++", "React"],
-      link: "#",
-      image: "/assets/images/cas.png",
-    },
-  ];
+  const router = useRouter();
+  const [projects] = useState<Project[]>(PROJECTS);
 
   return (
-    <div className="bg-primary min-h-screen p-8">
-      {/* TITLE */}
-      <div className="relative flex items-center justify-center mb-12 pb-10">
-        <h1 className="text-4xl font-bold text-center relative z-10 bg-primary px-4 text-accent">
-          My Projects
-        </h1>
-        <div className="absolute w-80 border-t-2 border-gray-300"></div>
-      </div>
-
-      <div className="flex flex-col gap-8 items-center">
-        {projects.map((project, index) => (
-          <div
-            key={index}
-            className={`relative flex flex-col md:flex-row py-12 ${
-              index % 2 !== 0 ? "md:flex-row-reverse" : ""
-            } max-w-7xl w-full rounded-lg shadow-md transition-shadow duration-300 overflow-hidden`}
+    <div className="relative flex flex-col min-h-screen w-full bg-gradient-primary items-center overflow-y-auto">
+      <NavBar />
+      <div className="flex w-full px-20 pb-10">
+        <div className="flex flex-col w-full py-[80px] px-[120px] bg-gradient-secondary rounded-[20px] relative">
+          <button
+            className="absolute top-4 left-4 p-6"
+            onClick={() => router.back()}
           >
-            <div className="absolute bottom-0 border-t-2 left-0 w-full border-accent"></div>
+            <Image
+              src="/assets/icons/back.svg"
+              height={32}
+              width={32}
+              alt="back"
+            />
+          </button>
 
-            {/* IMAGES */}
-            <div className="md:w-2/5">
-              <img
-                src={project.image}
-                alt={project.title}
-                className="object-cover w-full h-full"
-              />
-            </div>
+          {/* TITLE */}
+          <div className="relative flex flex-row items-center justify-center mb-10">
+            <h1 className="text-4xl font-bold text-center relative z-10 px-4 text-accent flex items-center">
+              <div className="w-[30px] border-t-2 border-gray-300 mr-4" />
+              My Projects
+              <div className="w-[30px] border-t-2 border-gray-300 ml-4" />
+            </h1>
+          </div>
 
-            {/* PROJECT DETAILS */}
-            <div className="flex flex-col justify-between p-6 md:w-3/5">
-              <div>
-                <h2 className="text-2xl font-semibold mb-4">{project.title}</h2>
-                <p className="text-text mb-4">{project.description}</p>
-                <div className="flex flex-wrap gap-2 mb-4">
-                  {/* TECH STACK*/}
-                  {project.technologies.map((tech, idx) => (
-                    <span
-                      key={idx}
-                      className="bg-secondary text-text py-2 px-4 rounded-full text-sm"
+          <div className="flex flex-col gap-2 items-center">
+            {projects.map((project, index) => (
+              <div
+                key={index}
+                className={`relative flex flex-col md:flex-row py-12 ${
+                  index % 2 !== 0 ? "md:flex-row-reverse" : ""
+                } max-w-7xl w-full rounded-lg shadow-md transition-shadow duration-300 overflow-hidden`}
+              >
+                <div className="absolute bottom-0 border-t-2 left-0 w-full border-accent"></div>
+
+                {/* IMAGES */}
+                <div className="md:w-2/5">
+                  <img
+                    src={project.image}
+                    alt={project.title}
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+
+                {/* PROJECT DETAILS */}
+                <div className="flex flex-col justify-between p-6 md:w-3/5">
+                  <div>
+                    <h2 className="text-2xl font-semibold mb-4">
+                      {project.title}
+                    </h2>
+                    <p className="text-text mb-4">{project.description}</p>
+                    <div className="flex flex-wrap gap-2 mb-4">
+                      {/* TECH STACK*/}
+                      {project.technologies.map((tech, idx) => (
+                        <span
+                          key={idx}
+                          className="bg-secondary text-text py-2 px-4 rounded-full text-sm"
+                        >
+                          {tech}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex flex-row space-x-5 mt-5">
+                    <a
+                      href={project.link}
+                      className="bg-accent py-2 px-4 rounded-lg inline-block w-24 text-primary text-center"
                     >
-                      {tech}
-                    </span>
-                  ))}
+                      GitHub
+                    </a>
+
+                    <a
+                      href={project.link}
+                      className="bg-primary border border-accent py-2 rounded-lg inline-block w-24 text-accent text-center"
+                    >
+                      Live Demo
+                    </a>
+                  </div>
                 </div>
               </div>
-              <div className="flex flex-row space-x-5 mt-5">
-                <a
-                  href={project.link}
-                  className="bg-accent hover:scale-105 transition-transform duration-300  py-2 px-4 rounded-lg inline-block w-24 text-primary text-center"
-                >
-                  GitHub
-                </a>
-
-                <a
-                  href={project.link}
-                  className="bg-primary border border-accent hover:scale-105 transition-transform duration-300 py-2 rounded-lg inline-block w-24 text-accent text-center"
-                >
-                  Live Demo
-                </a>
-              </div>
-            </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/app/utils/constants/projects.ts
@@ -0,0 +1,26 @@
+export const PROJECTS = [
+  {
+    title: "Project Kurdam",
+    description:
+      "A world of monsters and myths set in the University of the Philippines Visayas, Project Kurdam is a top-down pixel art action RPG game using Dungeons and Dragons comabt rules.",
+    technologies: ["Unity", "C#"],
+    link: "#",
+    image: "/assets/images/projectkurdam.png",
+  },
+  {
+    title: "DOST Customer Portal",
+    description:
+      "The DOST Region VI Customer Portal is designed to enhance customer interaction and service efficiency. It provides a user-friendly platform for customers to access various services, information, and support related to DOST Region VI.",
+    technologies: ["PHP", "Bulma CSS", "JavaScript"],
+    link: "#",
+    image: "/assets/images/cas.png",
+  },
+  {
+    title: "DOST Customer Portal",
+    description:
+      "The DOST Region VI Customer Portal is designed to enhance customer interaction and service efficiency. It provides a user-friendly platform for customers to access various services, information, and support related to DOST Region VI.",
+    technologies: ["Unity", "Textscript", "C++", "React"],
+    link: "#",
+    image: "/assets/images/cas.png",
+  },
+];
