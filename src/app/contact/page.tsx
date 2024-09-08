961d4f4294f3050e2dd187276fc6a997a7a6d4be
kryshyr <pbsaluria@up.edu.ph>
2024-09-08 20:57:41 +0800

    refactor: add navbar

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import NavBar from "@/components/common/navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import emailjs from 'emailjs-com';
-import React, { useState } from 'react';
+import emailjs from "emailjs-com";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import { SOCIALS } from "../utils/constants/socials";
 import { Social } from "../utils/types/socials";
 
-
 export default function Contact() {
+  const router = useRouter();
 
   const [socials] = useState<Social[]>(SOCIALS);
   const [formData, setFormData] = useState({
@@ -15,14 +18,14 @@ export default function Contact() {
     email: "",
     message: "",
   });
-  
 
   const [isSent, setIsSent] = useState(false);
   const isSubmitting = false;
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-
-    const {name, value} = e.target;
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
 
     setFormData({
       ...formData,
@@ -35,8 +38,8 @@ export default function Contact() {
 
     emailjs
       .send(
-        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || '',
-        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || '',
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "",
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "",
         formData,
         process.env.NEXT_PUBLIC_EMAILJS_USER_ID
       )
@@ -52,109 +55,122 @@ export default function Contact() {
   };
 
   return (
-    <div className="min-h-screen bg-primary text-xs flex items-center justify-center">
-      <div className="max-w-7xl w-full p-5 flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6">
-        <div className="flex-1 flex flex-col items-center justify-center text-center md:text-left">
-          <div className="text-white text-4xl md:text-5xl font-bold w-60">
-            Want to work
-          </div>
-          <div className="text-accent text-4xl md:text-5xl font-bold w-60">
-            together?
-          </div>
-        </div>
-        <div className="flex-1">
-          <div className="relative bg-gray-800 rounded-2xl shadow-lg z-10">
-            <div className="flex items-center pl-5 p-3 bg-secondary rounded-t-2xl">
-              <div className="flex-1 flex">
-                <div className="w-2 h-2 mr-3 rounded-full bg-cyan-400"></div>
-                <div className="w-2 h-2 mr-3 rounded-full bg-accent"></div>
-                <div className="w-2 h-2 rounded-full bg-cyan-200"></div>
-              </div>
+    <main className="relative flex flex-col min-h-screen w-full bg-gradient-primary items-center overflow-hidden">
+      <NavBar />
+      <button
+        className="absolute top-4 left-4 p-6"
+        onClick={() => router.back()}
+      >
+        <Image src="/assets/icons/back.svg" height={32} width={32} alt="back" />
+      </button>
+      <div className="flex w-full lg:px-20 px-8">
+        <div className="w-full p-5 flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6 bg-gradient-secondary py-20 rounded-[20px] ">
+          <div className="flex-1 flex flex-col items-center justify-center text-center md:text-left">
+            <div className="text-white text-4xl md:text-5xl font-bold w-60">
+              Want to work
             </div>
+            <div className="text-accent text-4xl md:text-5xl font-bold w-60">
+              together?
+            </div>
+          </div>
+          <div className="flex-1">
+            <div className="relative bg-gray-800 rounded-2xl shadow-lg z-10">
+              <div className="flex items-center pl-5 p-3 bg-secondary rounded-t-2xl">
+                <div className="flex-1 flex">
+                  <div className="w-2 h-2 mr-3 rounded-full bg-cyan-400"></div>
+                  <div className="w-2 h-2 mr-3 rounded-full bg-accent"></div>
+                  <div className="w-2 h-2 rounded-full bg-cyan-200"></div>
+                </div>
+              </div>
 
-            <div className="p-8 bg-primary border-4 rounded-b-2xl border-secondary">
-              <form onSubmit={sendEmail}>
-                <div className="flex flex-col space-y-6">
-                  <div className="flex flex-col md:flex-col lg:flex-row justify-between md:items-center">
-                    <div className="md:text-left lg:w-3/12">
-                      <div className="text-accent font-bold text-3xl md:text-4xl">
-                        <p className="mb-4">Contact</p>
+              <div className="p-8 bg-primary border-4 rounded-b-2xl border-secondary">
+                <form onSubmit={sendEmail}>
+                  <div className="flex flex-col space-y-6">
+                    <div className="flex flex-col md:flex-col lg:flex-row justify-between md:items-center">
+                      <div className="md:text-left lg:w-3/12">
+                        <div className="text-accent font-bold text-3xl md:text-4xl">
+                          <p className="mb-4">Contact</p>
+                        </div>
+                        <div className="text-accent font-bold text-3xl md:text-4xl w-14">
+                          <p className="mb-3 border-b-2 border-accent pb-1">
+                            Me
+                          </p>
+                        </div>
                       </div>
-                      <div className="text-accent font-bold text-3xl md:text-4xl w-14">
-                        <p className="mb-3 border-b-2 border-accent pb-1">Me</p>
+
+                      {/* FORM FIELDS */}
+                      <div className="md:w-7/12 space-y-6">
+                        <div className="pb-1 pt-5 border-b border-gray-600">
+                          <input
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="w-full bg-transparent text-sm text-gray-300 placeholder-gray-600 focus:outline-none"
+                            placeholder="NAME"
+                            required
+                          />
+                        </div>
+                        <div className="pb-1 border-b border-gray-600">
+                          <input
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            className="w-full bg-transparent text-sm text-gray-300 placeholder-gray-600 focus:outline-none"
+                            placeholder="EMAIL"
+                            required
+                          />
+                        </div>
+                        <div className="pb-1 border-b border-gray-600">
+                          <textarea
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            className="w-full bg-transparent text-sm text-gray-300 placeholder-gray-600 focus:outline-none"
+                            placeholder="MESSAGE"
+                            rows={3}
+                            required
+                          ></textarea>
+                        </div>
                       </div>
                     </div>
 
-                    {/* FORM FIELDS */}
-                    <div className="md:w-7/12 space-y-6">
-                      <div className="pb-1 pt-5 border-b border-gray-600">
-                        <input
-                          type="text"
-                          name="name"
-                          value={formData.name}
-                          onChange={handleChange}
-                          className="w-full bg-transparent text-sm text-gray-300 placeholder-gray-600 focus:outline-none"
-                          placeholder="NAME"
-                          required
-                        />
-                      </div>
-                      <div className="pb-1 border-b border-gray-600">
-                        <input
-                          type="email"
-                          name="email"
-                          value={formData.email}
-                          onChange={handleChange}
-                          className="w-full bg-transparent text-sm text-gray-300 placeholder-gray-600 focus:outline-none"
-                          placeholder="EMAIL"
-                          required
-                        />
-                      </div>
-                      <div className="pb-1 border-b border-gray-600">
-                        <textarea
-                          name="message"
-                          value={formData.message}
-                          onChange={handleChange}
-                          className="w-full bg-transparent text-sm text-gray-300 placeholder-gray-600 focus:outline-none"
-                          placeholder="MESSAGE"
-                          rows={3}
-                          required
-                        ></textarea>
-                      </div>
-                    </div>
-                  </div>
-
-                  {/* BUTTONS */}
-                  <div className="flex justify-end space-x-10 pb-4">
-                    {/* <button type="button" className="text-white hover:border-b-2 hover:border-accent border-b-2 border-transparent">
+                    {/* BUTTONS */}
+                    <div className="flex justify-end space-x-10 pb-4">
+                      {/* <button type="button" className="text-white hover:border-b-2 hover:border-accent border-b-2 border-transparent">
                       CANCEL
                     </button> */}
-                    <button disabled={isSubmitting} type="submit" className="text-accent hover:border-b-2 text-lg hover:border-accent border-b-2 border-transparent">
-                      {isSubmitting? 'Sending...' : 'SEND'}
-                    </button>
+                      <button
+                        disabled={isSubmitting}
+                        type="submit"
+                        className="text-accent hover:border-b-2 text-lg hover:border-accent border-b-2 border-transparent"
+                      >
+                        {isSubmitting ? "Sending..." : "SEND"}
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </form>
+                </form>
 
-              {/* SOCIAL MEDIA ICONS */}
-              <div className="flex md:justify-start justify-evenly space-x-4 h-7 mb-4">
-              {socials.map((social, index) => (
-                  <button
-                    key={index}
-                    onClick={() => window.open(social.url ?? "", "_blank")}
-                  >
-                    <FontAwesomeIcon
-                      icon={social.icon}
-                      className="h-[40px] w-[40px] hover:text-accent"
-                    />
-                  </button>
-                ))}
+                {/* SOCIAL MEDIA ICONS */}
+                <div className="flex md:justify-start justify-evenly space-x-4 h-7 mb-4">
+                  {socials.map((social, index) => (
+                    <button
+                      key={index}
+                      onClick={() => window.open(social.url ?? "", "_blank")}
+                    >
+                      <FontAwesomeIcon
+                        icon={social.icon}
+                        className="h-[40px] w-[40px] hover:text-accent"
+                      />
+                    </button>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    
+    </main>
   );
-  
 }
